b4afd8da0c177b4ef845f7180ee433db56de9723
Muhammadfahis Shareef <fahis.skazi@gmail.com>
2022-10-28 13:29:12 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import './style.scss'
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
+
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
